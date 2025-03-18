70340845d90392a7d4be832c87a618a5d4a5c9b2
jrd123 <31024338+jrd123@users.noreply.github.com>
2025-03-18 12:11:09 -0600

    Initial commit

---
+++ cassandraexample/src/main/java/com/jrd123/cassandraexample/CassandraexampleApplication.java
@@ -0,0 +1,13 @@
+package com.jrd123.cassandraexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraexampleApplication.class, args);
+	}
+
+}
